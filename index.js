07dd8ce18df2d6f520bf54d9b6505c3284b4dd0b
Victor Turitzin <turitzinv@gmail.com>
2021-11-24 18:49:27 -0500

    commit tree bc0a8915d2721b509a57727983211a33d26a92c9

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random())+1;
+
+let num3 = 22
+let num4 = 6
+
+const mod = num3 % num4;
+
+const max = Math.max(1,4,20,15);
